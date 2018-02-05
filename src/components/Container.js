2a501710b4c14bc489d1d0d4fc8588e074e48c2a
Nikolay Ivanov <nikolay9001@gmail.com>
2018-02-05 18:40:52 +0200

    Container component changed (not working yet)

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Container extends React.Component {
-//     var getInitialState = function() {
-//         return { setName: false }
-//     };
-//     var handleClick = function() {
-//         this.setState({ setName: true })
-//     };
-//     render() {
-//         var msg;
-//         if (this.state.setName) {
-//             msg = "Welcome to the chat"
-//         } else {
-//             msg = "Enter your name to join the chat"
-//         }
-//         return (
-//             <div>
-//                 <input type="text" />
-//                 <button onClick={this.handleClick}>Enter</button>
-//                 <p>{msg}</p>
-//             </div>
-//         );
-//     }
-// }
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { setName: false };
+    }
+
+    handleClick() {
+        this.setState({ setName: true });
+    }
+
+    render() {
+        var msg = '';
+        if (this.state.setName) {
+            msg = "Welcome to the chat"
+        } else {
+            msg = "Enter your name to join the chat"
+        }
+        return (
+            <div>
+                <input type="text" />
+                <button onClick={this.handleClick}>Enter</button>
+                <p>{msg}</p>
+            </div>
+        );
+    }
+}
 
 export default Container;
