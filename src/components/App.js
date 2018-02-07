3c553628f66ab9fdcbf17b7081c3186b5dc76816
Nikolay Ivanov <nikolay9001@gmail.com>
2018-02-07 09:50:45 +0200

    welcome message now shown correctly

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { setName: false };
+        this.state = { msg: "" };
 
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        this.setState({ setName: true });
-        // this.setState(prevState => ({
-        //     setName: !prevState.setName
-        // }));
+        if (this.refs.name.value) {
+            this.setState({ msg: "Welcome to the chat" });
+        } else {
+            this.setState({ msg: "Enter your name to join the chat" });
+        }
     }
 
     render() {
-        var msg = '';
-        if (this.state.setName) {
-            msg = "Welcome to the chat"
-        } else {
-            msg = "Enter your name to join the chat"
-        }
         return (
             <div>
-                <input type="text" />
+                <input type="text" ref="name" />
                 <button onClick={this.handleClick}>Enter</button>
-                <p>{msg}</p>
+                <p>{this.state.msg}</p>
             </div>
         );
     }
