65b7176601fe9ce2d12d2be94604d3264ca646f4
Nikolay Ivanov <nikolay9001@gmail.com>
2018-03-06 19:54:08 +0200

    style-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
                     loader: "css-loader" // translates CSS into CommonJS
                 }, {
                     loader: "sass-loader" // compiles Sass to CSS
